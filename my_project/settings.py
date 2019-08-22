d79d3f112a6928544b0860d354791a4f473b6b75
leobezze <leo.clnd@gmail.com>
2019-08-22 13:30:58 -0300

    Update settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -186,6 +186,8 @@ MIDDLEWARE_CLASSES = (
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
     'whitenoise.middleware.WhiteNoiseMiddleware',
-
+)
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
